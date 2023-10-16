f04423823be156acd30caa6dde3479db85075aac
Emiltjalol <Emil.ohrstrom@hotmail.com>
2023-10-16 09:10:00 +0200

    Testat att lägga till saker

---
+++ TjuvOchPolis/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TjuvOchPolis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nu har jag lagt till denna rad");
+        }
+    }
+}
